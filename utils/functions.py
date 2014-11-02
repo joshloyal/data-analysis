f4793d87c72379765580cdcca12418e9c038a6f7
joshloyal <jloyal25@gmail.com>
2014-11-02 17:28:14 -0500

    more changes to structure

--- utils/functions.py
+++
@@ -1,5 +0,0 @@
-def identity(x):
-    """ 
-    Placeholder for the identity function.
-    """
-    return x
--- utils/matrix.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def rec_to_mat(rec):
-    if rec.dtype.names is not None:
-        rec = rec.astype([(k, np.float64) for k in rec.dtype.names])
-        return np.mat(rec.view((np.float64, len(rec.dtype.names))))
-    else:
-        return np.matrix(rec.astype(np.float64))
